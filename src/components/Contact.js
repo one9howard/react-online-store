21861fd0615b90ba92c7068f532e4db7a3183ab1
jw <one9howard@hotmail.com>
2019-03-28 18:12:42 -0400

    my firt commit

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <center>
+          <h4>
+            <a href="https://www.instagram.com/reelbassackwards/">@Instagram</a>
+          </h4>
+          <br />
+          <h4>
+            <a href="https://www.instagram.com/reelbassackwards/">@Facebook</a>
+          </h4>
+          <br />
+          <h4>
+            <a href="https://www.instagram.com/reelbassackwards/">@Twitter</a>
+          </h4>
+          <br />
+          <img src="img/contactUs.png" alt="the boat" />
+        </center>
+      </div>
+    );
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "T-Shirts",
+    img: "img/product-1.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "comfortable blah blah blah",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Sweatshirts",
+    img: "img/product-2.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "this is a good shirt..so i hear",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Hoodies",
+    img: "img/product-3.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "It's a nice hoodie.. (and it looks comfortable)",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Sweatshirts (XL)",
+    img: "img/product-4.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "Reel in the big one in style with our comfortable sweatshirts!.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Lady Dry-Fit T's",
+    img: "img/product-5.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "it's pink.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Mens Dry-Fit T's",
+    img: "img/product-6.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "it's not pink.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "hat",
+    img: "img/product-7.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "it's a hat.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "hat",
+    img: "img/product-8.png",
+    price: 1.99,
+    company: "Bass'ackwards",
+    info: "another hat.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "",
+  img: "",
+  price: 0,
+  company: "",
+  info: "",
+  inCart: false,
+  count: 0,
+  total: 0
+};
